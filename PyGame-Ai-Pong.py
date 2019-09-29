7a7828c3f1a1af956cd71e48dbf5dbcb14124a2a
Justin Bennett <48428190+Justintb00@users.noreply.github.com>
2019-09-28 21:45:22 -0400

    Updated the game to implement classes

--- PyGame-Ai-Pong.py
+++ PyGame-Ai-Pong.py
@@ -3,20 +3,38 @@ pygame.init()
 
 win = pygame.display.set_mode((800,800)) #Windows Size
 pygame.display.set_caption("Pong")#Name of the Window
+class player:
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 20
+        self.color = color
+        self.isLost = False
 
-ax = 50
-ay = 50
-width = 10
-height = 70
-vel = 20
+class ball:
+    def __init__(self, x, y, radius, width, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.width = width
+        self.color = color
+        self.vel = 15
 
-bx = 750
-by = 50
 
 run = True
+
+p1 = player(50,50,10,70,(255,0,0))
+p2 = player(750,50,10,70,(0,0,255))
+ball = ball(200,200,3,3,(0,255,0))
+
 while run:
     
+    
     keys = pygame.key.get_pressed()
+    ball.x += ball.vel
+    ball.y += ball.vel
     
     pygame.time.delay(0)
     
@@ -25,27 +43,29 @@ while run:
             run = False
     
     if keys[pygame.K_UP]:
-        ay -= vel
+        p1.y -= p1.vel
     if keys[pygame.K_DOWN]:
-        ay += vel
+        p1.y += p1.vel
     if keys[pygame.K_w]:
-        by -= vel
+        p2.y -= p2.vel
     if keys[pygame.K_s]:
-        by += vel
-    if ay > 730 or ay < 0:
-        if ay > 730:
-            ay = 730
+        p2.y += p2.vel
+    if p1.y > 730 or p1.y < 0:
+        if p1.y > 730:
+            p1.y = 730
         else:
-            ay = 0
-    if by > 730 or by < 0:
-        if by > 730:    
-            by = 730
+            p1.y = 0
+    if p2.y > 730 or p2.y < 0:
+        if p2.y > 730:    
+            p2.y = 730
         else:
-            by = 0
+            p2.y = 0
     
     win.fill((0,0,0))
-    pygame.draw.rect(win, (255,0,0), (ax, ay, width, height))
-    pygame.draw.rect(win, (255,0,0), (bx, by, width, height))
+    pygame.draw.rect(win, p1.color, (p1.x, p1.y, p2.width, p2.height))
+    pygame.draw.rect(win, p2.color, (p2.x, p2.y, p2.width, p2.height))
+    pygame.draw.line(win, (255,255,255),(400,0),(400,800))
+    pygame.draw.circle(win, ball.color, (ball.x,ball.y) ,ball.radius, ball.width)
     pygame.display.update()
     
     
